b0bc01cfe610982e13bf444f99ce1da47183e3d6
Italo Maia <italo.maia@gmail.com>
2011-09-26 09:34:35 -0300

    Added default start_dir. Very useful.

--- flaskext/lettuce.py
+++ flaskext/lettuce.py
@@ -37,7 +37,7 @@ class Harvest(Command):
     """
     Harvest all features of the current application and run them
     """
-    def __init__(self, app=None, pattern='*/features', start_dir=None, verbosity=4):
+    def __init__(self, app=None, pattern='*/features', start_dir=".", verbosity=4):
         if start_dir is None:
             # Find the file that called this constructor and use its directory
             # as the start dir to scan for pattern
