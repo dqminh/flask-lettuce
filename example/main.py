603f64c39a58603d660ea716fc34548c40452f40
Italo Maia <italo.maia@gmail.com>
2011-10-19 13:46:49 -0300

    example now uses the config module for app configuration

--- example/main.py
+++ example/main.py
@@ -1,7 +1,10 @@
 # -*- coding:utf-8 -*-
 
+import config
+
 from flask import Flask
 
-def app_factory():
+def app_factory(config_obj=None):
     app = Flask(__name__)
+    app.config.from_object(config_obj or config.Dev)
     return app
--- example/manage.py
+++ example/manage.py
@@ -2,7 +2,9 @@ from flaskext.script import Manager
 from flaskext.lettuce import Harvest
 
 if __name__ == "__main__":
+    import config
     from main import app_factory
+
     manager = Manager(app_factory())
-    manager.add_command("harvest", Harvest(app_factory))
+    manager.add_command("harvest", Harvest(lambda: app_factory(config.Test)))
     manager.run()
