a0a6f0ca4b334acbb3a19b8364c3ff1b57c551b8
italo maia <italomaia@italomaia-1201N.(none)>
2011-11-17 16:39:34 -0300

    added new syntax for example manager creation.

--- example/manage.py
+++ example/manage.py
@@ -1,10 +1,11 @@
-from flaskext.script import Manager
-from flaskext.lettuce import Harvest
+from flask.ext.script import Manager
+from flask.ext.lettuce import Harvest
 
 if __name__ == "__main__":
     import config
     from main import app_factory
 
-    manager = Manager(app_factory())
+    manager = Manager(app_factory)
+    manager.add_option("-c", "--config", dest="config_obj", default=config.Dev, required=False)
     manager.add_command("harvest", Harvest(lambda: app_factory(config.Test)))
     manager.run()
